db994b05982f6ea61a41f958c9b86242472824ce
zbl14 <ifthereisoneday@gmail.com>
2022-07-10 16:24:11 -0700

    add Dictionary riddles with one riddle

---
+++ Sphinx/Models/Riddle.cs
@@ -0,0 +1,7 @@
+namespace Sphinx.Models;
+{
+  public static Dictionary<string, string> riddles = new Dictionary<string, string>() 
+  {
+    {"1", "What goes on four legs in the morning, two legs in the afternoon, and three legs in the evening? The answer? Man"}
+  };
+}
