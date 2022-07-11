f93092a944aba4b00ef6b052e7eb9505da1509ae
zbl14 <ifthereisoneday@gmail.com>
2022-07-10 22:43:15 -0700

    randomized riddles

--- Sphinx/Program.cs
+++ Sphinx/Program.cs
@@ -8,17 +8,21 @@ namespace Sphinx
  {
   public static void Main()
   {
+    Random rnd = new Random();
+    int i = rnd.Next(13);
+    Console.WriteLine("=======================");
     Console.WriteLine("Welcome to Sphinx World");
-    Console.WriteLine(Riddle.riddles.ElementAt(0).Value);
+    Console.WriteLine(Riddle.riddles.ElementAt(i).Value);
     Console.WriteLine("What is your answer?");
     string answer = Console.ReadLine();
-    if (answer == Riddle.riddles.ElementAt(0).Key)
+    if (answer.ToLower() == Riddle.riddles.ElementAt(i).Key.ToLower())
     {
-      Console.WriteLine("You are correct!");
+      Console.WriteLine("You are corret!")
+      Main();
     }
     else
     {
-      Main();
+      Console.WriteLine("Game Over!");
     }
   }
  } 
