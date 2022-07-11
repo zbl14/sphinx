8eead766452b4eac8d88e0e48f3056bb93e6bd56
zbl14 <ifthereisoneday@gmail.com>
2022-07-10 20:04:13 -0700

    add public class Riddle

--- Sphinx/Models/Riddle.cs
+++ Sphinx/Models/Riddle.cs
@@ -1,7 +1,12 @@
-namespace Sphinx.Models;
+using System.Collections.Generic;
+
+namespace Sphinx.Models
 {
-  public static Dictionary<string, string> riddles = new Dictionary<string, string>() 
+  public class Riddle
   {
-    {"Man", "What goes on four legs in the morning, two legs in the afternoon, and three legs in the evening?"}
-  };
+    public static Dictionary<string, string> riddles = new Dictionary<string, string>() 
+    {
+      {"Man", "What goes on four legs in the morning, two legs in the afternoon, and three legs in the evening?"}
+    };
+  }
 }
