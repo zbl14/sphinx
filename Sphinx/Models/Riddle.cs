77b6aac1efc7dc5d8bb5e4fdd0293a4dae162477
zbl14 <ifthereisoneday@gmail.com>
2022-07-10 20:20:43 -0700

    add more riddles

--- Sphinx/Models/Riddle.cs
+++ Sphinx/Models/Riddle.cs
@@ -6,7 +6,20 @@ namespace Sphinx.Models
   {
     public static Dictionary<string, string> riddles = new Dictionary<string, string>() 
     {
-      {"Man", "What goes on four legs in the morning, two legs in the afternoon, and three legs in the evening?"}
+      {"Man", "What goes on four legs in the morning, two legs in the afternoon, and three legs in the evening?"},
+      {"Day and Night", "There are two sisters: one gives birth to the other and she, in turn, gives birth to the first. What are they?"},
+      {"Sun", "Never resting, never still; moving silently from hill to hill; it does not walk, run or trot; all is cool where it is not. What is it?"},
+      {"Hummingbird","As small as your thumb, I am light in the air. You may hear me before you see me, but trust that I'm here. What is it?"},
+      {"Tree","In spring I am gay in handsome array; in summer more clothing I wear; when colder it grows, I fling off my clothes; and in winter quite naked appear. What is it?"},
+      {"Memory","What can bring back the dead; make you cry, make you laugh, make you young; is born in an instant, yet lasts a lifetime."},
+      {"Moon","What is always old and sometimes new; never sad, sometimes blue; never empty, but sometimes full; never pushes, always pulls?"},
+      {"Mountain", "What is large, yet never grows; has roots that cannot be seen; and is taller than trees?"},
+      {"Stars", "At night they come without being fetched, and by day they are lost without being stolen. What are they?"},
+      {"Spider", "I create my lair with earthen string, and dispatch my prey with a biting sting. What is it?"},
+      {"Time", "This thing all things devour: birds, beasts, trees, flowers; gnaws iron, bites steel; grinds hard stones to meal. What is it?"},
+      {"Death", "Some try to hide, some try to cheat; but time will show, we always will meet. Try as you might to guess my name."},
+      {"River", "What can run, but never walks; has a mouth, but never talks; has a head, but never weeps; has a bed, but never sleeps?"},
+      {"Fish", "I'm alive, but without breath; I'm as cold in life as in death; I'm never thirsty, though I always drink. Who am I?"}
     };
   }
 }
